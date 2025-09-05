5e5a80c3c27aa5664d3f69f7a5d4486861dbddfc
Jonatas Felipe <jonatas-nandes@hotmail.com>
2025-09-05 18:45:33 -0300

    Ajustes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { loadRemote } from '@module-federation/runtime';
+
 
 import RouteWrapper from './RouteWrapper';
 
 import Login from '~/pages/Login';
 
-const UsersApp = lazy(() => import('topUsers/App'));
-const FinanceApp = lazy(() => import('topFinance/App'));
+const UsersApp = lazy(() => loadRemote<any>('topUsers/App').then((module) => ({ default: module })),);
+const FinanceApp = lazy(() => loadRemote<any>('topFinance/App').then((module) => ({ default: module })),);
 
 const routes: React.FC = () => {
   return (
